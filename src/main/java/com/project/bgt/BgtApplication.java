d1f6c09a58fc209d6d7fb5f5be94c0808a1c7b4c
MantasDemenius <mantas.demenius@gmail.com>
2020-09-15 20:28:07 +0300

    Initial commit

---
+++ src/main/java/com/project/bgt/BgtApplication.java
@@ -0,0 +1,13 @@
+package com.project.bgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgtApplication.class, args);
+	}
+
+}
