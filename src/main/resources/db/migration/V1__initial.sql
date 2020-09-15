194563b43bdb573879d24ea40a5476e24970743c
MantasDemenius <mantas.demenius@gmail.com>
2020-09-15 20:54:20 +0300

    flyway setup

---
+++ src/main/resources/db/migration/V1__initial.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS card
+(
+    id BIGSERIAL PRIMARY KEY,
+    title TEXT,
+    description  TEXT
+);
---
+++ src/main/resources/db/migration/V2__game.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS game
+(
+    "id" BIGSERIAL PRIMARY KEY,
+    "name" TEXT,
+    description  TEXT
+);
