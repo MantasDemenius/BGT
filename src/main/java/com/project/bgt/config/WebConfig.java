8b761c47104733fd4417daa5fcfae5545fcb5745
MantasDemenius <mantas.demenius@gmail.com>
2020-12-07 20:25:28 +0200

    Allowed remote

--- src/main/java/com/project/bgt/config/WebConfig.java
+++ src/main/java/com/project/bgt/config/WebConfig.java
@@ -13,6 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
       .allowedOrigins("http://localhost:3000")
+      .allowedOrigins("https://mantasdemenius.github.io/BGT-frontend/")
       .allowedMethods("GET", "POST", "PUT", "DELETE");
   }
 }
